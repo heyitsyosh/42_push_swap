a2380cd05584c6dc1ef563640a92cd8fc2229f58
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-10-12 18:50:36 +0900

    update

--- push_swap/mandatory/push_swap.c
+++ push_swap/mandatory/push_swap.c
@@ -6,29 +6,33 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 15:43:55 by myoshika          #+#    #+#             */
-/*   Updated: 2022/10/12 16:35:15 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/10/12 18:48:23 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	sa(int *a, t_save *s)
+void	sa(int *a, t_save *s, bool need_to_print)
 {
 	if (s->a_top < 1)
 		return ;
-
+	if (need_to_print)
+		ft_printf("sa\n");
 }
 
-void	sb(int *b, t_save *s)
+void	sb(int *b, t_save *s, bool need_to_print)
 {
 	if (s->b_top < 1)
 		return ;
+	if (need_to_print)
+		ft_printf("sb\n");
 }
 
 void	ss(int *a, int *b, t_save *s)
 {
-	sa(a, s);
-	sb(b, s);
+	sa(a, s, NO_PRINTF);
+	sb(b, s, NO_PRINTF);
+	ft_printf("pb\n");
 }
 
 void	pa(t_save *s)
@@ -40,6 +44,7 @@ void	pa(t_save *s)
 	s->b_top--;
 	
 	s->a_top++;
+	ft_printf("pa\n");
 }
 
 void	pb(int *a, int *b, t_save *s)
@@ -47,4 +52,5 @@ void	pb(int *a, int *b, t_save *s)
 	if (s->a_top == -1)
 		return ;
 	s->a_top++;
+	ft_printf("pb\n");
 }
