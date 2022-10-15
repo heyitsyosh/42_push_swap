3b48308e25ff8cac3bffef77c098e6dfca92d8d4
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-10-16 01:04:59 +0900

    update

--- push_swap/mandatory/lst_funcs.c
+++ push_swap/mandatory/lst_funcs.c
@@ -6,21 +6,11 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/13 13:34:42 by myoshika          #+#    #+#             */
-/*   Updated: 2022/10/16 01:04:42 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/10/16 01:04:53 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-t_stack	*stack_last(t_stack *head)
-{
-	t_stack	*tmp;
-
-	tmp = head;
-	while (tmp->next)
-		tmp = tmp->next;
-	return (tmp);
-}
 
 void	stack_add_back(t_stack *node, t_stack **tail)
 {
