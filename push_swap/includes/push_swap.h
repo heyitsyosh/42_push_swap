5c4eff98bc44b8b7dafe16abfa3d0005f49245fd
myoshika <47554536+heyitsyosh@users.noreply.github.com>
2022-10-15 23:36:15 +0900

    update

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 23:12:15 by myoshika          #+#    #+#             */
-/*   Updated: 2022/10/15 23:20:48 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/10/15 23:35:48 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ typedef struct s_save
 {
 	t_stack	*a_head;
 	t_stack	*b_head;
+	t_stack	*a_tail;
+	t_stack	*b_tail;
 	t_stack	*lmis_i;
 	t_stack	*lmis_ii;
 	t_stack	*lmis_iii;
@@ -53,8 +55,8 @@ int		atoi_with_overflow_check(const char *str, bool *overflow);
 void	get_lmis_and_compressed_coordinates(t_save *s);
 
 t_stack	*stack_last(t_stack *head);
-void	stack_add_back(t_stack *node, t_stack **head);
-void	stack_add_front(t_stack *node, t_stack **head);
+void	stack_add_back(t_stack *node, t_stack **head, t_stack **tail);
+void	stack_add_front(t_stack *node, t_stack **head, t_stack **tail);
 void	free_a_and_b(t_save *s);
 
 void	sort(t_save *s);
