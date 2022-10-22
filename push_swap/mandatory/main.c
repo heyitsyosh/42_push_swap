f9c6f460bc1d4ea6a87135ae9ef713bb70031489
maria <winterzaq1@gmail.com>
2022-10-23 06:14:52 +0900

    update

--- push_swap/mandatory/main.c
+++ push_swap/mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 23:09:58 by myoshika          #+#    #+#             */
-/*   Updated: 2022/10/23 05:05:13 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/10/23 06:09:30 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void tmp_print(t_save *s)
 	printf("a:");
 	while (next)
 	{
-		printf("[%d]", next->input);
+		printf("[%d]", next->cc);
 		fflush(stdout);
 		next = next->next;
 	}
@@ -28,7 +28,7 @@ void tmp_print(t_save *s)
 	printf("b:");
 	while (next)
 	{
-		printf("[%d]", next->input);
+		printf("[%d]", next->cc);
 		fflush(stdout);
 		next = next->next;
 	}
