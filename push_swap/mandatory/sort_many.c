d6b665d40c4af7818dd4c1bef086eaa7a0713402
maria <winterzaq1@gmail.com>
2022-11-15 21:57:53 +0900

    ahhh

--- push_swap/mandatory/sort_many.c
+++ push_swap/mandatory/sort_many.c
@@ -6,54 +6,76 @@
 /*   By: myoshika <myoshika@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 23:46:18 by myoshika          #+#    #+#             */
-/*   Updated: 2022/11/15 20:47:51 by myoshika         ###   ########.fr       */
+/*   Updated: 2022/11/15 21:56:13 by myoshika         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-static bool	should_rr(int pivot, t_save *s)
+static void	exit_if_sorted(t_save *s)
 {
-	if (s->b_size <= 1)
-		return (false);
-	if (pivot != s->argc / 2)
-		return (false);
-	if ()
-	return (true);
+	t_stack	*next;
+
+	if (s->a_size != (s->argc - 1) && s->b_size > 0)
+		return ;
+	next = s->a_head->next;
+	while (next)
+	{
+		if ((next->prev)->cc > next->cc)
+			return ;
+		next = next->next;
+	}
+	free_nodes(s);
+	exit(EXIT_SUCCESS);
 }
 
-static void	divide(int pivot, int sorted, t_save *s)
+static void	divide(int pivot, int actions, int confirmed_size, t_save *s)
 {
-	while (s->a_head && sorted < pivot)
+	int	a_size;
+
+	a_size = s->a_size;
+	while (s->a_head && s-> < pivot && actions < a_size)
 	{
+		// tmp_print(s, NULL);
+		fflush(stdout);
 		if (s->a_head->cc < pivot && !s->a_head->part_of_lis)
 		{
 			pb(s, PB);
-			sorted++;
+			pushed++;
 		}
 		else
 		{
-			if (s->a_head->part_of_lis)
-				sorted++;
-			if (should_rr(pivot, s))
-				rr(s, RR);
-			else
-				ra(s, RA);
+			actions++;
+			ra(s, RA);
 			exit_if_sorted(s);
 		}
 	}
 	if (s->a_size > s->lis->i_s_len + 1)
-		divide(sorted + (s->argc - sorted) / 2, sorted, s);
+		divide(pivot + (pivot + 1) / 2, 0, confirmed_size, s);
 }
+
+// int	swap_opportunity();
+
+// void	combine(t_save *s)
+// {
+// 	int	tail_cost;
+// 	int	swap_opportunity;
+
+// 	while (s->b_size != 0)
+// 	{
+// 		swap_opportunity(s);
+// 	}
+// }
 
 void	sort_many(t_save *s)
 {
-	// tmp_print_lis(s, NULL);
-	// tmp_print_bool(s, NULL);
-	// tmp_print(s, NULL);
-	// printf("[b pivot:%d]\n", (s->argc - 1 - s->lis->i_s_len) / 2);
-	// printf("[lds:%d | len:%d]\n", s->lis->cc, s->lis->i_s_len);
-	// printf("[lds:%d]\n", s->lis_end->cc);
-	// fflush(stdout);
-	divide(s->argc / 2, 0, s);
+	tmp_print_lis(s);
+	tmp_print_bool(s);
+	tmp_print(s, NULL);
+	printf("[b pivot:%d]\n", s->argc / 2);
+	printf("[lds:%d | len:%d]\n", s->lis->cc, s->lis->i_s_len);
+	printf("[lds:%d]\n", s->lis_end->cc);
+	fflush(stdout);
+	divide(s->argc / 2, 0, 0, s);
+	//combine(s);
 }
